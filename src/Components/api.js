8689af4d2f57510529f2f74de31298c6944bc9c2
SarvagnyaDesh <sarvagnyadesh1008@gmail.com>
2025-01-09 10:48:37 +0530

    apdated api

--- src/Components/api.js
+++ src/Components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/', // Replace with your backend URL
+  baseURL: 'https://cowsandbullswebgame.onrender.com', // Replace with your backend URL
 });
 
 export default api;
